fe1260c0d61b004f813ca48ed0372039d17b2be2
herrnorbert <nkobli@yahoo.com>
2017-10-17 20:12:09 +0200

    fix()

--- workshop.py
+++ workshop.py
@@ -7,9 +7,6 @@ width, height = image.size
 for i in range(0, width):
     for j in range(0, height):
         r, g, b = pixel_matrix[i,j]
-        r + 100
-        g + 100
-        b + 100
-        pixel_matrix[i,j] = (r, g, b)
+        pixel_matrix[i,j] = (r + 100, g + 100, b + 100)
         
 image.save("light.png")
